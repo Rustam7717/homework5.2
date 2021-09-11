681ab34d4d76b941470f82f411011f77c8b48308
Rustam7717 <rustam85@mail.ru>
2021-09-11 22:16:21 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner test = new Scanner(System.in);
+        System.out.println("Input your name:");
+        String name = test.nextLine();
+        System.out.println("Thanks! You are welcome mrs " + name + "!!");
+
+
+    }
+}
